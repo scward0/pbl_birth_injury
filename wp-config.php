18df366b528c843392343db11e7b80fc066bb379
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-26 14:11:02 -0700

    db update

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'localwp');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'SilverH3elm1!');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
