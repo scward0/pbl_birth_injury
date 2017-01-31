2a562487d1bfcbcd20ba774f646a971e1856fa21
Sean Ward <scward0@gmail.com>
2017-01-31 13:57:20 -0700

    single practice area

--- wp-content/themes/FoundationPress/single-practice_area.php
+++ wp-content/themes/FoundationPress/single-practice_area.php
@@ -1,30 +1,18 @@
-<div class="container">
-  <div style="background: url('<?php the_field('hero'); ?>'); height: 50%; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover;" >
-    <?php get_header(); ?>
+<div class="container" style="background: url('<?php the_field('background_image'); ?>'); background-size: cover;" >
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php get_header(); ?>
+  <div class="row text-center" >
+    <h1 class="text-center page-title"><?php the_title(); ?> <br><span class="title-hr">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h1>
+  </div>
+  <div class="row">
+    <div class="columns small-12 medium-12 large-8" id="about">
+      <?php the_field('description'); ?>
+    </div>
+    <div id="form" class="columns small-12 medium-12 large-4">
+        <?php the_field('ninja'); ?>
+    </div>
   </div>
 </div>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-
-  <div class="container white">
-    <div class="row">
-      <div class="columns small-12 medium-12 large-8" id="about">
-        <h3 class="text-center"><?php the_title(); ?></h3>
-        <?php the_field('description'); ?>
-      </div>
-      <div id="form" class="columns small-12 medium-12 large-4">
-          <?php the_field('ninja'); ?>
-      </div>
-    </div>
-    <div class="row">
-      <div class="columns small-12 medium-12 large-12">
-        <div class="embed-container" style="padding: 10%;">
-	         <?php the_field('video'); ?>
-         </div>
-      </div>
-    </div>
-  </div>
 
 <?php endwhile; else : ?>
 
