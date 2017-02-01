26c38b2a65e429df7a0c73cc6670cc608cc1019b
Sean Ward <scward0@gmail.com>
2017-02-01 11:41:19 -0700

    align middle

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -128,7 +128,7 @@
 
 <section id="video" style="background: url('http://198.58.102.147/pbl_birth_injury/wp-content/uploads/2017/01/CharlieVideoBackground.jpg'); background-size: cover; height: 600px;">
   <div class="row text-center align-middle" style="vertical-align: middle;">
-    <div class="columns small-12 medium-12 large-12">
+    <div class="columns small-12 medium-12 large-12 align-middle" style="vertical-align: middle;">
       <span class="title-hr">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
       <p style="font-size: 52px; color: white;">
         Charlie's Angels
