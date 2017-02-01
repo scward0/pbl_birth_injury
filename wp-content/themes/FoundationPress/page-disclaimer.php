693f9c4c825b4a26259d6a0fc269009085aa088c
Sean Ward <scward0@gmail.com>
2017-02-01 15:23:39 -0700

    page disclaimer

---
+++ wp-content/themes/FoundationPress/page-disclaimer.php
@@ -0,0 +1,28 @@
+<div class="container" style="background: url('<?php the_field('background_image'); ?>'); background-size: cover;" >
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php get_header(); ?>
+  <div class="row text-center" >
+    <h1 class="text-center page-title"><?php the_title(); ?> <br><span class="title-hr">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h1>
+  </div>
+  <div class="row">
+    <div class="columns small-12 medium-12 large-12" id="about">
+      <?php the_field('content'); ?> <br>
+      <?php the_field('google_map'); ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="columns small-12 medium-12 large-12">
+      <div class="embed-container" style="padding: 10%;">
+         <?php the_field('video'); ?>
+       </div>
+    </div>
+  </div>
+</div>
+
+<?php endwhile; else : ?>
+
+<p><?php _e( 'Sorry, no pages found' ); ?></p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
