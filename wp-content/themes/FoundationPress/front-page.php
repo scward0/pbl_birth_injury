b607e8e233d31f771ed249dd4112dd2742b7e82a
Sean Ward <scward0@gmail.com>
2017-02-01 11:46:53 -0700

    charlies margin

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -127,8 +127,8 @@
 </section>
 
 <section id="video" style="background: url('http://198.58.102.147/pbl_birth_injury/wp-content/uploads/2017/01/CharlieVideoBackground.jpg'); background-size: cover; height: 600px;">
-  <div class="row text-center align-middle" style="vertical-align: middle;">
-    <div class="columns small-12 medium-12 large-12 align-middle" style="vertical-align: middle;">
+  <div class="row text-center align-middle">
+    <div class="columns small-12 medium-12 large-12 charlies-angels">
       <span class="title-hr">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
       <p style="font-size: 52px; color: white;">
         Charlie's Angels
