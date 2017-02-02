e12aa7d2d0251c90d24f70f9558783da0c133cae
Sean Ward <scward0@gmail.com>
2017-02-02 13:04:39 -0700

    blog date

--- wp-content/themes/FoundationPress/single-blog_post.php
+++ wp-content/themes/FoundationPress/single-blog_post.php
@@ -1,12 +1,12 @@
 <div class="container" style="background: url('<?php the_field('background_image'); ?>'); background-size: cover;" >
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <?php get_header(); ?>
-  <div class="row text-center" id="blog-post"> >
+  <div class="row text-center" id="blog-post">
     <h1 class="text-center page-title"><?php the_title(); ?> <br><span class="title-hr">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h1>
   </div>
   <div class="row">
     <div class="columns small-12 medium-12 large-8" id="blog-post">
-        <div id="date"><?php echo get_field('date'); ?></div>
+        <div id="date"><?php echo get_field('date'); ?></div><br>
         <div id='entry'><?php echo get_field('entry'); ?></div>
     </div>
     <div id="form" class="columns small-12 medium-12 large-4">
