cc01ce8085638f9dc49047827e548efcabb2742e
Sean Ward <scward0@gmail.com>
2017-02-03 09:56:50 -0700

    category pages

--- wp-content/themes/FoundationPress/page-blog-birth-injuries.php
+++ wp-content/themes/FoundationPress/page-blog-birth-injuries.php
@@ -24,7 +24,8 @@
         ?>
 
       <?php if( $query->have_posts() ) : while($query->have_posts() ) : $query->the_post(); ?>
-        <?php if( get_field('category') == 3 ); ?>
+        <?php $cat = get_field("category"); ?>
+        <?php if( $cat == 3 ); ?>
           <h4><?php the_title(); ?></h4>
           <div id="date">
             <?php the_field('date'); ?>
