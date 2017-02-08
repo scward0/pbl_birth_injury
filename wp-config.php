02a6ea69a8fa658a17670299aed1bedde1947564
Sean Ward <scward0@gmail.com>
2017-02-08 10:49:19 -0700

    migrate database

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,26 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'localwp');
+// define('DB_NAME', 'localwp');
+//
+// /** MySQL database username */
+// define('DB_USER', 'root');
+//
+// /** MySQL database password */
+// define('DB_PASSWORD', 'SilverH3elm1!');
+
+
+
+define('DB_NAME', 'pblbirthinjury');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'SilverH3elm1!');
+define('DB_PASSWORD', 's!nCe1886');
+
+
+
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
