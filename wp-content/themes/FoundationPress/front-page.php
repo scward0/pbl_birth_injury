6e7effcea9f5da53a7250546de53a4b0b2169848
Sean Ward <scward0@gmail.com>
2017-02-23 15:29:57 -0700

    pillars links

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -40,18 +40,21 @@
      <div class="block text-center">
        <img src="http://198.58.102.147/pbl_birth_injury/wp-content/uploads/2017/01/Experience.jpg" alt="" />
        <h3 >Experience<br><br><span class="smaller">Led by Charles Thronson, the Personal Injury practice group at Parsons Behle & Latimer has more than thirty years of experience representing clients in birth injury, cerebral palsy, and severe child injury cases.</span></h3>
+       <a class="read-more" href="http://198.58.102.147/pbl_birth_injury/about/">Learn about Charles Thronson</a>
      </div>
    </div>
    <div class="small-12 medium-12 large-4 columns">
      <div class="block text-center">
        <img src="http://198.58.102.147/pbl_birth_injury/wp-content/uploads/2017/01/Results.jpg" alt="" />
        <h3>Results<br><br><span class="smaller">We represent innocent victims of medical negligence and believe injured children and their families deserve the best possible legal representation possible. Our track record includes achieving a number of multi-million dollar settlements for our clients.</span></h3>
+       <a class="read-more" href="http://198.58.102.147/pbl_birth_injury/our-families/">See Our Results</a>
      </div>
    </div>
    <div class="small-12 medium-12 large-4 columns">
      <div class="block text-center">
        <img src="http://198.58.102.147/pbl_birth_injury/wp-content/uploads/2017/01/Integrity.jpg" alt="" />
-       <h3>Integrity<br><br><span class="smaller">Our clients face enormous financial and emotional hardships as a result of medical malpractice. Our team works hand-in-hand to assess the economic impact of birth injuries on both the child and their family.</span></h3>
+       <h3>Integrity<br><br><span class="smaller">Parsons Behle & Latimer is one of the 300 largest firms in America. Our size, reputation and resources give us a distinct advantage when handling birth injuries and injured children cases.</span></h3>
+       <a class="read-more" href="http://198.58.102.147/pbl_birth_injury/parsons-behle-latimer/">Read about the Firm</a>
      </div>
    </div>
  </section>
