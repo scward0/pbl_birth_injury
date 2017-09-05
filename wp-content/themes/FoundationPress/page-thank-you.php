1323e0e091b10f2d92e15b1dcaa2a61d07e55685
Sean Ward <scward0@gmail.com>
2017-09-05 11:42:31 -0600

    centro

--- wp-content/themes/FoundationPress/page-thank-you.php
+++ wp-content/themes/FoundationPress/page-thank-you.php
@@ -1,11 +1,4 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-35693720-3', 'auto');
-  ga('send', 'pageview');
-</script>
+<?php get_header(); ?>
 
 <div class="container" style="background: url('<?php the_field('background_image'); ?>'); background-size: cover;" >
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
